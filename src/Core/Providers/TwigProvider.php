9db34cab20b90f2b6c62f87dd684e96791034018
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-12-29 22:40:56 +0000

    Added Twig

---
+++ src/Core/Providers/TwigProvider.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Core\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class TwigProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        'Twig_Environment',
+    ];
+
+    public function register(): void
+    {
+        // Register items
+        $this->getContainer()
+             ->add('Twig_Environment', function () {
+                 $loader = new \Twig_Loader_Filesystem(BASE_DIR.'/src/views');
+                 $twig = new \Twig_Environment($loader, array(
+                     'cache' => BASE_DIR.'/cache/views',
+                 ));
+                 return $twig;
+             });
+    }
+}
