3e689a0707e4d17a02b04956bf0ffa5cce1a8237
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-12-29 16:48:05 +0000

    Added index file

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+if (!defined('BASE_DIR')) {
+    define('BASE_DIR', __DIR__.'/../');
+}
+
+error_reporting(E_ALL);
+
+$dotenv = new Dotenv\Dotenv(BASE_DIR);
+$dotenv->load();
+if (getenv('APP_ENV') == 'development') {
+    ini_set('display_errors', '1');
+} else {
+    ini_set('display_errors', '0');
+    ini_set('log_errors', '1');
+}
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+ 
+$app = new Statico\Core\Kernel;
+$response = $app->bootstrap()
+    ->handle($request);
+$app->getContainer()->get('emitter')->emit($response);
