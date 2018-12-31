4ed2644bdf70aab158975bdb59575c63b207e748
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-12-31 22:39:27 +0000

    Added basic console helper

---
+++ src/Core/Console/Server.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Core\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Server extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('server')
+            ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Which port should the server run on?')
+            ->setDescription('Runs the development server')
+            ->setHelp('This command runs the development server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        if (!$port = $input->getOption('port')) {
+            $port = 8000;
+        }
+        $output->writeln('Running PHP development server on port '.$port.'...');
+        passthru('php -S localhost:'.$port.' -t '.getcwd().'/public');
+    }
+}
---
+++ src/Core/Console/Shell.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Core\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Container\ContainerInterface;
+use Psy\Shell as Psysh;
+use Psy\Configuration;
+
+class Shell extends Command
+{
+    protected $container;
+
+    protected $shell;
+    
+    public function __construct(ContainerInterface $container, Psysh $shell)
+    {
+        parent::__construct();
+        $this->container = $container;
+        $this->shell = $shell;
+    }
+    
+    protected function configure(): void
+    {
+        $this->setName('shell')
+             ->setDescription('Runs an interactive shell')
+             ->setHelp('This command runs an interactive shell');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $container = $this->container;
+
+        $this->shell->run();
+    }
+}
