c190600e585513a29b497f9a1a7a60072614ce25
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-12-31 22:07:13 +0000

    Fixed index route

--- routes.php
+++ routes.php
@@ -1,3 +1,4 @@
 <?php declare(strict_types = 1);
 
+$router->get('/', 'Statico\Core\Http\Controllers\MainController::index');
 $router->get('/{name:word}', 'Statico\Core\Http\Controllers\MainController::index');
