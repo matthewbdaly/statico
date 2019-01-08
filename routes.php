bccb11ccda4f6b845807702cd968c21db01b3048
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-01-08 22:59:22 +0000

    Added ETag middleware

--- routes.php
+++ routes.php
@@ -1,4 +1,6 @@
 <?php declare(strict_types = 1);
 
-$router->get('/', 'Statico\Core\Http\Controllers\MainController::index');
-$router->get('/{name:[a-zA-Z0-9\-\/]+}', 'Statico\Core\Http\Controllers\MainController::index');
+$router->get('/', 'Statico\Core\Http\Controllers\MainController::index')
+    ->middleware(new \Statico\Core\Http\Middleware\ETag);
+$router->get('/{name:[a-zA-Z0-9\-\/]+}', 'Statico\Core\Http\Controllers\MainController::index')
+    ->middleware(new \Statico\Core\Http\Middleware\ETag);
---
+++ src/Core/Http/Middleware/ETag.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Core\Http\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class ETag implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        $method = $request->getMethod();
+
+        // If this was not a GET or HEAD request, just return
+        if ($method != 'GET' && $method != 'HEAD') {
+            return $response;
+        }
+        $etag = md5($response->getBody()->getContents());
+        $requestEtag = str_replace('"', '', $request->getHeader('if-none-match'));
+
+        // Check to see if Etag has changed
+        if ($requestEtag && $requestEtag[0] == $etag) {
+            return $response->withStatus(304);
+        }
+
+        // Set Etag
+        return $response->withAddedHeader('etag', $etag);
+    }
+}
