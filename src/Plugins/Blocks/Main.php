1d6fa4b7ca16fb45843e3debca921423ab0265a9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-01-08 22:29:53 +0000

    Added placeholders for three more plugins

---
+++ src/Plugins/Blocks/Main.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Plugins\Blocks;
+
+use Statico\Core\Contracts\Extension\Plugin;
+
+final class Main implements Plugin
+{
+    public function register()
+    {
+        // Register plugin
+    }
+}
---
+++ src/Plugins/Sitemap/Main.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Plugins\Sitemap;
+
+use Statico\Core\Contracts\Extension\Plugin;
+
+final class Main implements Plugin
+{
+    public function register()
+    {
+        // Register plugin
+    }
+}
