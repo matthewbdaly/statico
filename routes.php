b84e6e9dbc01d7ed9990cc388870f7696df930c3
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-01-09 00:01:22 +0000

    Refactored routes

--- routes.php
+++ routes.php
@@ -1,6 +1,4 @@
 <?php declare(strict_types = 1);
 
-$router->get('/', 'Statico\Core\Http\Controllers\MainController::index')
-    ->middleware(new \Statico\Core\Http\Middleware\ETag);
-$router->get('/{name:[a-zA-Z0-9\-\/]+}', 'Statico\Core\Http\Controllers\MainController::index')
+$router->get('/[{name:[a-zA-Z0-9\-\/]+}]', 'Statico\Core\Http\Controllers\MainController::index')
     ->middleware(new \Statico\Core\Http\Middleware\ETag);
