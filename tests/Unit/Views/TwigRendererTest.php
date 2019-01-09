5a2597b3063d955a4b88786a836980827406027e
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-01-09 22:28:37 +0000

    Added test for Twig renderer

---
+++ tests/Unit/Views/TwigRendererTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Views;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Core\Views\TwigRenderer;
+
+class TwigRendererTest extends TestCase
+{
+    public function testRenderer(): void
+    {
+        $twig = m::mock('Twig_Environment');
+        $twig->shouldReceive('load')->with('foo.html')->once()->andReturn($twig);
+        $twig->shouldReceive('render')->with(['Foo'])->once()->andReturn(['Foo']);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')->once()->andReturn($response);
+        $response->shouldReceive('write')->once()->andReturn($response);
+        $renderer = new TwigRenderer($twig);
+        $renderer->render($response, 'foo.html', ['Foo']);
+    }
+}
