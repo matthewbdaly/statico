ae764341fa94c88289e973da5bb1848a57324000
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-01-09 22:19:49 +0000

    Added test for log handler

---
+++ tests/Unit/Exceptions/LogHandlerTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Exceptions;
+
+use Tests\TestCase;
+use Statico\Core\Exceptions\LogHandler;
+use Mockery as m;
+use Exception;
+
+class LogHandlerTest extends TestCase
+{
+    public function testHandleException(): void
+    {
+        $logger = m::mock('Psr\Log\LoggerInterface');
+        $logger->shouldReceive('error');
+        $inspector = m::mock('Whoops\Exception\Inspector');
+        $run = m::mock('Whoops\RunInterface');
+        $handler = new LogHandler($logger);
+        $e = new Exception('Test message');
+        $handler($e, $inspector, $run);
+    }
+}
