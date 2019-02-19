02a4af83d600eda554ac863dd44e31169b03a800
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-19 17:39:36 +0000

    Tests in place for Etag middleware

---
+++ tests/Unit/Http/Middleware/ETagTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Http\Middleware;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Core\Http\Middleware\ETag;
+
+class ETagTest extends TestCase
+{
+    public function testNotGet()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getMethod')->andReturn('POST');
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')->with($request)->andReturn($response);
+        $middleware = new ETag();
+        $received = $middleware->process($request, $handler);
+        $this->assertEquals($received, $response);
+    }
+
+    public function testGet()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getMethod')->andReturn('GET');
+        $request->shouldReceive('getHeader')->with('if-none-match')->andReturn([
+            md5('foo')
+        ]);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody->getContents')->andReturn('foo');
+        $response->shouldReceive('withAddedHeader')->andReturn($response);
+        $response->shouldReceive('withStatus')->with(304)->andReturn($response);
+        $handler = m::mock('Psr\Http\Server\RequestHandlerInterface');
+        $handler->shouldReceive('handle')->with($request)->andReturn($response);
+        $middleware = new ETag();
+        $received = $middleware->process($request, $handler);
+    }
+}
