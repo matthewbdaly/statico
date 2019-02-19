d3124a113dcf335dce7ae68b26e9301be4198034
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-19 16:50:43 +0000

    Fixed a return type

--- src/Core/Paths/LocalResolver.php
+++ src/Core/Paths/LocalResolver.php
@@ -6,7 +6,7 @@ use Statico\Core\Contracts\Paths\Resolver;
 
 final class LocalResolver implements Resolver
 {
-    public function resolve(string $name)
+    public function resolve(string $name): ?string
     {
         $filename = BASE_DIR.CONTENT_PATH. rtrim($name, '/') . '.md';
         if (!file_exists($filename)) {
