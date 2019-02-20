1ad1263dd9d4408a23a0e4b78b394336cd2591ec
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-20 21:29:44 +0000

    Finished tests for data source

--- tests/Unit/Core/Sources/MarkdownFileSourceTest.php
+++ tests/Unit/Core/Sources/MarkdownFileSourceTest.php
@@ -11,7 +11,27 @@ class MarkdownFileSourceTest extends TestCase
     public function testAll()
     {
         $manager = m::mock('League\Flysystem\MountManager');
+        $manager->shouldReceive('listContents')
+            ->with('content://', true)
+            ->once()
+            ->andReturn([[
+                'type' => 'dir',
+                'path' => 'about'
+            ], [
+                'type' => 'file',
+                'path' => 'foo.md'
+            ]]);
+        $manager->shouldReceive('read')->with('content://foo.md')
+            ->once()
+            ->andReturn('foo');
+        $document = m::mock('Mni\FrontYAML\Document');
+        $document->shouldReceive('getYAML')->once()
+            ->andReturn([
+                'title' => 'Foo'
+            ]);
         $parser = m::mock('Mni\FrontYAML\Parser');
+        $parser->shouldReceive('parse')->with('foo')->once()
+            ->andReturn($document);
         $source = new MarkdownFiles($manager, $parser);
         $source->all();
     }
@@ -31,6 +51,20 @@ class MarkdownFileSourceTest extends TestCase
             ->andReturn($document);
         $source = new MarkdownFiles($manager, $parser);
         $this->assertEquals($document, $source->find('foo'));
+    }
+
+    public function testFindEmpty()
+    {
+        $manager = m::mock('League\Flysystem\MountManager');
+        $manager->shouldReceive('has')->with('content://foo.md')
+            ->once()
+            ->andReturn(true);
+        $manager->shouldReceive('read')->with('content://foo.md')
+            ->once()
+            ->andReturn(null);
+        $parser = m::mock('Mni\FrontYAML\Parser');
+        $source = new MarkdownFiles($manager, $parser);
+        $this->assertNull($source->find('foo'));
     }
 
     public function testFindNull()
