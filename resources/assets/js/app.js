be3f9d0d789fd41c61a98037701ae21da1022f13
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-20 16:18:46 +0000

    Added JS for Fuse

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,38 @@
+import Fuse from 'fuse.js';
+
 try {
   window.$ = window.jQuery = require('jquery');
 
   require('bootstrap');
+
+  $(document).ready(function () {
+    window.$.getJSON('/storage/index.json', function (response) {
+      const fuse = new Fuse(response, {
+        keys: ['title'],
+        id: 'path'
+      });
+      $('#search').on('keyup', function () {
+        let result = fuse.search($(this).val());
+
+        // Output it
+        let resultdiv = $('ul.searchresults');
+        if (result.length === 0) {
+          // Hide results
+          resultdiv.hide();
+        } else {
+          // Show results
+          resultdiv.empty();
+          for (let item in result.slice(0,4)) {
+            let ref = result[item].ref;
+            let searchitem = '<li><a href="' + ref + '">' + store[ref].title + '</a></li>';
+            resultdiv.append(searchitem);
+          }
+          resultdiv.show();
+        }
+
+      });
+    });
+  });
 } catch (e) {
   console.log(e);
 }
