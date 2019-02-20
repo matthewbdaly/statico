75bbb4235dc9296bf952a18c9d05308ad4dc4f82
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-20 12:30:18 +0000

    Added a missing test

---
+++ tests/Unit/Providers/ConfigProviderTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Providers;
+
+use Tests\TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    public function testCreateContainer(): void
+    {
+        $config = $this->container->get('Zend\Config\Reader\ReaderInterface');
+        $this->assertInstanceOf('Zend\Config\Reader\ReaderInterface', $config);
+        $this->assertInstanceOf('Zend\Config\Reader\Yaml', $config);
+    }
+}
