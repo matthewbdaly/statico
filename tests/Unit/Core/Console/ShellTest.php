99de6e7c9b3705e3b04233d2e99fb87f59e66758
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-02-21 21:27:16 +0000

    Added a test for the shell command

---
+++ tests/Unit/Core/Console/ShellTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Core\Console;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Core\Console\Shell;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ShellTest extends TestCase
+{
+    public function testExecute()
+    {
+        $container = m::mock('Psr\Container\ContainerInterface');
+        $shell = m::mock('Psy\Shell');
+        $shell->shouldReceive('run')
+            ->once()
+            ->andReturn(true);
+        $cmd = new Shell($container, $shell);
+        $tester = new CommandTester($cmd);
+        $tester->execute([]);
+    }
+}
