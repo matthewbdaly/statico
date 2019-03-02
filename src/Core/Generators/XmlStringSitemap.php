afc60d562279222a51eebf6dec82dbce233fab82
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-03-02 21:30:33 +0000

    Sitemap now picks up base URL

--- src/Core/Generators/XmlStringSitemap.php
+++ src/Core/Generators/XmlStringSitemap.php
@@ -6,18 +6,25 @@ use SimpleXmlElement;
 use Statico\Core\Traits\ParsesPath;
 use Statico\Core\Contracts\Sources\Source;
 use Statico\Core\Contracts\Generators\Sitemap;
+use Zend\Config\Config;
 
 final class XmlStringSitemap implements Sitemap
 {
     use ParsesPath;
 
     /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
      * @var Source
      */
     protected $source;
 
-    public function __construct(Source $source)
+    public function __construct(Config $config, Source $source)
     {
+        $this->config = $config;
         $this->source = $source;
     }
 
@@ -27,7 +34,7 @@ final class XmlStringSitemap implements Sitemap
         $xml = new SimpleXMLElement("<?xml version='1.0' encoding='UTF-8' ?>\n".'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" />');
         foreach ($documents as $document) {
             $item = $xml->addChild('url');
-            $item->addChild('loc', $this->parsePath($document->getPath()));
+            $item->addChild('loc', $this->config->get('base_url') . '/' . $this->parsePath($document->getPath()));
         }
         return $xml->asXml();
     }
--- tests/Unit/Core/Generators/SitemapTest.php
+++ tests/Unit/Core/Generators/SitemapTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Mockery as m;
 use Statico\Core\Objects\Document;
 use Statico\Core\Generators\XmlStringSitemap;
+use Zend\Config\Config;
 
 class SitemapTest extends TestCase
 {
@@ -13,7 +14,7 @@ class SitemapTest extends TestCase
     {
         $expectedResponse = <<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>foo/</loc></url></urlset>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://mysite.com/foo/</loc></url></urlset>
 EOF;
         $doc = new Document;
         $doc->setContent('This is my content');
@@ -25,7 +26,12 @@ EOF;
         $source->shouldReceive('all')->once()->andReturn([
             $doc
         ]);
-        $generator = new XmlStringSitemap($source);
+        $config = m::mock('Zend\Config\Config');
+        $config->shouldReceive('get')
+            ->with('base_url')
+            ->once()
+            ->andReturn('https://mysite.com');
+        $generator = new XmlStringSitemap($config, $source);
         $response = $generator();
         $this->assertEquals(trim($expectedResponse), trim($response));
     }
