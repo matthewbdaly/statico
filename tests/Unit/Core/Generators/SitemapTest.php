54cfe5b79e72a7e6da9f3ac48a74f43b02d13e88
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-03-02 20:30:07 +0000

    Can now generate sitemap

--- tests/Unit/Core/Generators/SitemapTest.php
+++ tests/Unit/Core/Generators/SitemapTest.php
@@ -12,12 +12,8 @@ class SitemapTest extends TestCase
     public function testExecute()
     {
         $expectedResponse = <<<EOF
-<?xml version="1.0"?>
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://mysite.com/foo/</loc>
-  </url>
-</urlset>
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>foo/</loc></url></urlset>
 EOF;
         $doc = new Document;
         $doc->setContent('This is my content');
@@ -31,6 +27,6 @@ EOF;
         ]);
         $generator = new XmlStringSitemap($source);
         $response = $generator();
-        $this->assertEquals($expectedResponse, $response);
+        $this->assertEquals(trim($expectedResponse), trim($response));
     }
 }
