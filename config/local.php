8fc2d5758289d0f4d0dd22bd9e4c2921765c4de9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-03-03 15:41:24 +0000

    Amended config to pick up env file

---
+++ config/local.php
@@ -0,0 +1,5 @@
+<?php declare(strict_types = 1);
+
+return [
+    'base_url' => getenv('BASE_URL'),
+];
--- src/Core/Providers/ConfigProvider.php
+++ src/Core/Providers/ConfigProvider.php
@@ -20,7 +20,7 @@ final class ConfigProvider extends AbstractServiceProvider
             ->share('Zend\Config\Config', function () {
                 $parser = $this->getContainer()->get('Statico\Core\Utilities\YamlWrapper');
                 Factory::registerReader('yml', new Yaml($parser));
-                return new Config(Factory::fromFile('config.yml'));
+                return new Config(Factory::fromFiles(glob('config/*.*')));
             });
     }
 }
