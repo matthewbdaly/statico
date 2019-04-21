b6236174be6d489be049a41ee9c3181dcb8b33be
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-04-21 20:44:48 +0100

    Added a missing return type hint

--- src/Core/Contracts/Sources/Source.php
+++ src/Core/Contracts/Sources/Source.php
@@ -2,9 +2,11 @@
 
 namespace Statico\Core\Contracts\Sources;
 
+use Statico\Core\Objects\Document;
+
 interface Source
 {
     public function all(): array;
 
-    public function find(string $name);
+    public function find(string $name): ?Document;
 }
