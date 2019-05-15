efb25d6c19d4a10f1a45abc85c4e8949d1a53029
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-05-15 16:11:40 +0100

    Form helper extended

--- src/Core/Views/Functions/Form.php
+++ src/Core/Views/Functions/Form.php
@@ -2,10 +2,26 @@
 
 namespace Statico\Core\Views\Functions;
 
+use Zend\Config\Config;
+use Statico\Core\Exceptions\Forms\FormNotFound;
+
 final class Form
 {
+    /**
+     * @var Config;
+     */
+    private $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config->get('forms');
+    }
+
     public function __invoke(string $form): string
     {
+        if (!isset($this->config[$form])) {
+            throw new FormNotFound('The specified form is not registered');
+        }
         return '';
     }
 }
