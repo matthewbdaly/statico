1def26085bdcc564604d3acc22ca60741fa35821
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-05-19 09:59:58 +0100

    Added value object for email address

---
+++ src/Core/Objects/EmailAddress.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Statico\Core\Objects;
+
+use Statico\Core\Exceptions\Objects\EmailAddressInvalid;
+
+final class EmailAddress
+{
+    /**
+     * @var string
+     */
+    private $email;
+
+    public function __construct(string $email)
+    {
+        if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
+            throw new EmailAddressInvalid('The provided email address is not valid');
+        }
+        $this->email = $email;
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    public function equals(EmailAddress $email): bool
+    {
+        return ($this->email === $email->__toString());
+    }
+}
---
+++ tests/Unit/Core/Objects/EmailAddressTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Core\Objects;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Core\Objects\EmailAddress;
+
+class EmailAddressTest extends TestCase
+{
+    public function testCreate()
+    {
+        $email = "bob@example.com";
+        $obj = new EmailAddress($email);
+        $this->assertEquals($email, $obj->__toString());
+    }
+
+    public function testFailedCreate()
+    {
+        $this->expectException('Statico\Core\Exceptions\Objects\EmailAddressInvalid');
+        $email = "example.com";
+        $obj = new EmailAddress($email);
+    }
+
+    public function testEquals()
+    {
+        $obj = new EmailAddress('bob@example.com');
+        $this->assertTrue($obj->equals(new EmailAddress('bob@example.com')));
+        $this->assertFalse($obj->equals(new EmailAddress('eric@example.com')));
+    }
+}
