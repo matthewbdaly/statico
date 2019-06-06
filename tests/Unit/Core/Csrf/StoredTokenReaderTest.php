f40538474c001036a8a5e8591c37ad8de708b2cd
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-06-06 21:26:43 +0100

    Test in place for token reader

---
+++ tests/Unit/Core/Csrf/StoredTokenReaderTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Core\Csrf;
+
+use Tests\TestCase;
+use Statico\Core\Csrf\StoredTokenReader;
+use Statico\Core\Csrf\Token;
+use Mockery as m;
+
+class StoredTokenReaderTest extends TestCase
+{
+    public function testReadToken()
+    {
+        $storage = m::mock('Statico\Core\Csrf\TokenStorage');
+        $token = new Token('bar');
+        $storage->shouldReceive('retrieve')
+            ->with('foo')
+            ->once()
+            ->andReturn($token);
+        $reader = new StoredTokenReader($storage);
+        $this->assertEquals($token, $reader->read('foo'));
+    }
+
+    public function testGenerateToken()
+    {
+        $storage = m::mock('Statico\Core\Csrf\TokenStorage');
+        $storage->shouldReceive('retrieve')
+            ->with('foo')
+            ->once()
+            ->andReturn(null);
+        $storage->shouldReceive('store')->once();
+        $reader = new StoredTokenReader($storage);
+        $this->assertInstanceOf(Token::class, $reader->read('foo'));
+    }
+}
