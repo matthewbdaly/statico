0951b45b84dca5fd7aa0dde1d053d1b515d0324d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-06-09 22:22:18 +0100

    Added skipping for tests
    Any tests with missing dependencies for the Cache factory test case are
    now skipped

--- tests/Unit/Core/Factories/CacheFactoryTest.php
+++ tests/Unit/Core/Factories/CacheFactoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Core\Factories;
 
 use Tests\TestCase;
 use Statico\Core\Factories\CacheFactory;
+use Stash\Exception\RuntimeException;
 use Mockery as m;
 
 final class CacheFactoryTest extends TestCase
@@ -29,9 +30,13 @@ final class CacheFactoryTest extends TestCase
     public function testApc()
     {
         $factory = new CacheFactory;
-        $pool = $factory->make([
-            'driver' => 'apc'
-        ]);
+        try {
+            $pool = $factory->make([
+                'driver' => 'apc'
+            ]);
+        } catch (RuntimeException $e) {
+            $this->markTestSkipped('Dependency not installed');
+        }
         $this->assertInstanceOf('Stash\Pool', $pool);
         $this->assertInstanceOf('Stash\Driver\Apc', $pool->getDriver());
     }
@@ -39,9 +44,13 @@ final class CacheFactoryTest extends TestCase
     public function testMemcache()
     {
         $factory = new CacheFactory;
-        $pool = $factory->make([
-            'driver' => 'memcache'
-        ]);
+        try {
+            $pool = $factory->make([
+                'driver' => 'memcache'
+            ]);
+        } catch (RuntimeException $e) {
+            $this->markTestSkipped('Dependency not installed');
+        }
         $this->assertInstanceOf('Stash\Pool', $pool);
         $this->assertInstanceOf('Stash\Driver\Memcache', $pool->getDriver());
     }
@@ -49,12 +58,16 @@ final class CacheFactoryTest extends TestCase
     public function testRedis()
     {
         $factory = new CacheFactory;
-        $pool = $factory->make([
-            'driver' => 'redis',
-            'servers' => [
-                '127.0.0.1',
-            ]
-        ]);
+        try {
+            $pool = $factory->make([
+                'driver' => 'redis',
+                'servers' => [
+                    '127.0.0.1',
+                ]
+            ]);
+        } catch (RuntimeException $e) {
+            $this->markTestSkipped('Dependency not installed');
+        }
         $this->assertInstanceOf('Stash\Pool', $pool);
         $this->assertInstanceOf('Stash\Driver\Redis', $pool->getDriver());
     }
