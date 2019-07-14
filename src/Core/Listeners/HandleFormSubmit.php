81f6a466e0ae6934e8039bc482716afb53d9d313
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-07-14 20:13:14 +0100

    Can now resolve listener

---
+++ src/Core/Listeners/HandleFormSubmit.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Core\Listeners;
+
+use League\Event\EventInterface;
+
+final class HandleFormSubmit extends BaseListener
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(EventInterface $event)
+    {
+    }
+}
