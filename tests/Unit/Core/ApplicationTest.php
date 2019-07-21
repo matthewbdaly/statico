31b5021dd99be4c3a9057ab752576287f71d8fe6
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-07-22 00:22:28 +0100

    Fixed a coverage gap

---
+++ tests/Unit/Core/ApplicationTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Core\Utilities;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Core\Application;
+
+final class ApplicationTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        putenv('APP_ENV=production');
+    }
+
+    public function tearDown(): void
+    {
+        putenv('APP_ENV=testing');
+        parent::tearDown();
+    }
+
+    public function testErrorHandler()
+    {
+        $handler = m::mock('Statico\Core\Contracts\Exceptions\Handler');
+        $router = m::mock('League\Route\Router');
+        $router->shouldReceive('get')
+            ->andReturn($router);
+        $router->shouldReceive('middleware')
+            ->andReturn($router);
+        $router->shouldReceive('post')
+            ->andReturn($router);
+        $container = m::mock('League\Container\Container');
+        $container->shouldReceive('delegate')->once();
+        $container->shouldReceive('addServiceProvider');
+        $container->shouldReceive('share')->times(2);
+        $container->shouldReceive('get')->with('League\Route\Router')
+            ->once()
+            ->andReturn($router);
+        $container->shouldReceive('get')->with('Statico\Core\Contracts\Exceptions\Handler')
+            ->once()
+            ->andReturn($handler);
+        $app = new Application($container);
+        $app->bootstrap();
+    }
+}
