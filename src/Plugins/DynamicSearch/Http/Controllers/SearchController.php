a9cfb5e4a3acf339fde86fd67f7d851f51b22840
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-08-18 16:56:22 +0100

    Added controller and set up route for dynamic search

---
+++ src/Plugins/DynamicSearch/Http/Controllers/SearchController.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Statico\Plugins\DynamicSearch\Http\Controllers;
+
+use Statico\Core\Contracts\Sources\Source;
+use Zend\Diactoros\Response\JsonResponse;
+
+final class SearchController
+{
+    /**
+     * @var Source
+     */
+    private $source;
+
+    public function __construct(Source $source)
+    {
+        $this->source = $source;
+    }
+
+    public function index()
+    {
+        $searchable = $this->source->all();
+        return new JsonResponse(
+            $searchable->toArray(),
+            200,
+            ['Content-Type' => ['application/hal+json']]
+        );
+    }
+}
--- src/Plugins/DynamicSearch/Plugin.php
+++ src/Plugins/DynamicSearch/Plugin.php
@@ -2,6 +2,27 @@
 
 namespace Statico\Plugins\DynamicSearch;
 
+use League\Route\Router;
+
 final class Plugin
 {
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function register()
+    {
+        $this->registerRoute();
+    }
+
+    private function registerRoute()
+    {
+        $this->router->get('/index.json', 'Statico\Plugins\DynamicSearch\Http\Controllers\SearchController::index');
+    }
 }
