678d3dba8601e7bf9a8cc1d2a48f6a48dc1345c4
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-08-18 18:03:44 +0100

    Added test for second plugin

---
+++ tests/Unit/Plugins/DynamicSitemap/PluginTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Plugins\DynamicSitemap;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Plugins\DynamicSitemap\Plugin;
+
+final class PluginTest extends TestCase
+{
+    public function testSetup()
+    {
+        $router = m::mock('League\Route\Router');
+        $plugin = new Plugin($router);
+        $router->shouldReceive('get')
+            ->with('/sitemap', 'Statico\Plugins\DynamicSitemap\Http\Controllers\SitemapController::index')
+            ->once();
+        $plugin->register();
+    }
+}
