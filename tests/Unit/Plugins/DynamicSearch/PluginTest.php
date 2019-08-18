98127b99b4f54fd952c9029a7d7c35812279a291
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-08-18 18:01:17 +0100

    Added a test for one of the plugins

---
+++ tests/Unit/Plugins/DynamicSearch/PluginTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Plugins\DynamicSearch;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Plugins\DynamicSearch\Plugin;
+
+final class PluginTest extends TestCase
+{
+    public function testSetup()
+    {
+        $router = m::mock('League\Route\Router');
+        $plugin = new Plugin($router);
+        $router->shouldReceive('get')
+            ->with('/search/index', 'Statico\Plugins\DynamicSearch\Http\Controllers\SearchController::index')
+            ->once();
+        $plugin->register();
+    }
+}
