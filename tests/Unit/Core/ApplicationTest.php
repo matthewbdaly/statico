e235b514042269478021b6742eed08fc23f5dcdd
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-08-18 20:38:23 +0100

    Added tests for checks on plugins

--- tests/Unit/Core/ApplicationTest.php
+++ tests/Unit/Core/ApplicationTest.php
@@ -47,4 +47,56 @@ final class ApplicationTest extends TestCase
         $app = new Application($container);
         $app->bootstrap();
     }
+
+    public function testPluginNotFound()
+    {
+        $this->expectException('Statico\Core\Exceptions\Plugins\PluginNotFound');
+        $handler = m::mock('Statico\Core\Contracts\Exceptions\Handler');
+        $container = m::mock('League\Container\Container');
+        $container->shouldReceive('delegate')->once();
+        $container->shouldReceive('addServiceProvider');
+        $container->shouldReceive('share')->times(2);
+        $container->shouldReceive('get')->with('My\Nonexistent\Plugin')
+            ->once()
+            ->andReturn(null);
+        $container->shouldReceive('get')->with('Statico\Core\Contracts\Exceptions\Handler')
+            ->once()
+            ->andReturn($handler);
+        $config = new Config([
+            'plugins' => [
+                'My\Nonexistent\Plugin'
+            ]
+        ]);
+        $container->shouldReceive('get')->with('Zend\Config\Config')
+            ->once()
+            ->andReturn($config);
+        $app = new Application($container);
+        $app->bootstrap();
+    }
+
+    public function testPluginNotValid()
+    {
+        $this->expectException('Statico\Core\Exceptions\Plugins\PluginNotValid');
+        $handler = m::mock('Statico\Core\Contracts\Exceptions\Handler');
+        $container = m::mock('League\Container\Container');
+        $container->shouldReceive('delegate')->once();
+        $container->shouldReceive('addServiceProvider');
+        $container->shouldReceive('share')->times(2);
+        $container->shouldReceive('get')->with('stdClass')
+            ->once()
+            ->andReturn(new \stdClass());
+        $container->shouldReceive('get')->with('Statico\Core\Contracts\Exceptions\Handler')
+            ->once()
+            ->andReturn($handler);
+        $config = new Config([
+            'plugins' => [
+                'stdClass'
+            ]
+        ]);
+        $container->shouldReceive('get')->with('Zend\Config\Config')
+            ->once()
+            ->andReturn($config);
+        $app = new Application($container);
+        $app->bootstrap();
+    }
 }
