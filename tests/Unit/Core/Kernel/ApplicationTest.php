b3fa2532cdb024c1091a7c1f1f570974e00f4dfb
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-09-02 22:13:01 +0100

    Fixed coverage gaps in main application kernel

--- tests/Unit/Core/Kernel/ApplicationTest.php
+++ tests/Unit/Core/Kernel/ApplicationTest.php
@@ -18,6 +18,7 @@ final class ApplicationTest extends TestCase
     public function tearDown(): void
     {
         putenv('APP_ENV=testing');
+        error_reporting(E_ALL);
         parent::tearDown();
     }
 
@@ -98,5 +99,43 @@ final class ApplicationTest extends TestCase
             ->andReturn($config);
         $app = new Application($container);
         $app->bootstrap();
+    }
+
+    public function testClockwork()
+    {
+        putenv('APP_ENV=development');
+        if (defined('E_STRICT')) {
+            error_reporting('E_ALL | E_STRICT');
+        }
+        $handler = m::mock('Statico\Core\Contracts\Exceptions\Handler');
+        $clockwork = m::mock('Clockwork\Support\Vanilla\Clockwork')->makePartial();
+        $clockwork->shouldReceive('requestProcessed')->once();
+        $container = m::mock('League\Container\Container');
+        $container->shouldReceive('delegate')->once();
+        $container->shouldReceive('addServiceProvider');
+        $container->shouldReceive('share');
+        $container->shouldReceive('get')->with('Clockwork\Support\Vanilla\Clockwork')
+            ->once()
+            ->andReturn($clockwork);
+        $router = m::mock('League\Route\Router');
+        $router->shouldReceive('get')
+            ->with('/__clockwork/{request:.+}', 'Statico\Core\Http\Controllers\ClockworkController::process')
+            ->once()
+            ->andReturn($router);
+        $router->shouldReceive('get')->andReturn($router);
+        $router->shouldReceive('middleware')->andReturn($router);
+        $router->shouldReceive('post')->andReturn($router);
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $router->shouldReceive('dispatch')->andReturn($response);
+        $container->shouldReceive('get')->with('League\Route\Router')
+            ->once()
+            ->andReturn($router);
+        $container->shouldReceive('get')->with('Zend\Config\Config')
+            ->once()
+            ->andReturn(new Config([]));
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $app = new Application($container);
+        $app->bootstrap();
+        $this->assertEquals($response, $app->handle($request));
     }
 }
