2a518b688a9f34058fa2858cdbe20f9f0b76ec4e
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-09-03 21:12:42 +0100

    Finished tests for HTTP cache
    Still need to test the store

--- tests/Unit/Core/Kernel/HttpCache/HttpCacheTest.php
+++ tests/Unit/Core/Kernel/HttpCache/HttpCacheTest.php
@@ -31,7 +31,6 @@ final class HttpCacheTest extends TestCase
         $request->shouldReceive('getMethod')
             ->once()
             ->andReturn('GET');
-        $response = m::mock('Psr\Http\Message\ResponseInterface');
         $kernel = m::mock('Statico\Core\Contracts\Kernel\KernelInterface');
         $store = m::mock('Statico\Core\Contracts\Kernel\HttpCache\StoreInterface');
         $store->shouldReceive('get')
@@ -41,5 +40,57 @@ final class HttpCacheTest extends TestCase
         $response = $cache->handle($request);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals("foo", $response->getBody()->getContents());
+    }
+
+    public function testGetNon200()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getMethod')
+            ->once()
+            ->andReturn('GET');
+        $mockResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $mockResponse->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn(201);
+        $kernel = m::mock('Statico\Core\Contracts\Kernel\KernelInterface');
+        $kernel->shouldReceive('handle')
+            ->with($request)
+            ->once()
+            ->andReturn($mockResponse);
+        $store = m::mock('Statico\Core\Contracts\Kernel\HttpCache\StoreInterface');
+        $store->shouldReceive('get')
+            ->once()
+            ->andReturn(null);
+        $cache = new HttpCache($kernel, $store);
+        $response = $cache->handle($request);
+        $this->assertSame($response, $mockResponse);
+    }
+
+    public function testGet200()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $request->shouldReceive('getMethod')
+            ->once()
+            ->andReturn('GET');
+        $mockResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $mockResponse->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn(200);
+        $kernel = m::mock('Statico\Core\Contracts\Kernel\KernelInterface');
+        $kernel->shouldReceive('handle')
+            ->with($request)
+            ->once()
+            ->andReturn($mockResponse);
+        $store = m::mock('Statico\Core\Contracts\Kernel\HttpCache\StoreInterface');
+        $store->shouldReceive('get')
+            ->with($request)
+            ->once()
+            ->andReturn(null);
+        $store->shouldReceive('put')
+            ->with($request, $mockResponse)
+            ->once();
+        $cache = new HttpCache($kernel, $store);
+        $response = $cache->handle($request);
+        $this->assertSame($response, $mockResponse);
     }
 }
