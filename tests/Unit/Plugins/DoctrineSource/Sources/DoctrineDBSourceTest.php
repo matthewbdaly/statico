8d819811d18e7afb5461aa24e637ff52826cd2d3
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-09-07 18:14:32 +0100

    Added a test for the Doctrine source file

---
+++ tests/Unit/Plugins/DoctrineSource/Sources/DoctrineDBSourceTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Plugins\DoctrineSource\Sources;
+
+use Tests\TestCase;
+use Mockery as m;
+use Statico\Plugins\DoctrineSource\Sources\DoctrineDB;
+use Statico\Core\Utilities\Collection;
+
+final class DoctrineDBSourceTest extends TestCase
+{
+    public function testAll()
+    {
+        $document = m::mock('Statico\Plugins\DoctrineSource\Entities\DoctrineDocument');
+        $repo = m::mock('Doctrine\Common\Persistence\ObjectRepository');
+        $repo->shouldReceive('findAll')->once()->andReturn([$document]);
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $em->shouldReceive('getRepository')->once()->andReturn($repo);
+        $source = new DoctrineDB($em);
+        $response = $source->all();
+        $this->assertInstanceOf('Statico\Core\Utilities\Collection', $response);
+        $this->assertEquals($document, $response[0]);
+    }
+
+    public function testFind()
+    {
+        $document = m::mock('Statico\Plugins\DoctrineSource\Entities\DoctrineDocument');
+        $repo = m::mock('Doctrine\Common\Persistence\ObjectRepository');
+        $repo->shouldReceive('findOneBy')
+            ->with(['path' => 'foo/'])
+            ->once()
+            ->andReturn($document);
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $em->shouldReceive('getRepository')->once()->andReturn($repo);
+        $source = new DoctrineDB($em);
+        $response = $source->find('foo/');
+        $this->assertEquals($document, $response);
+    }
+}
