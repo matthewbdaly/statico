a54a6f416e5cc543798fc7f9e180efc5f28d55e6
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-09-25 21:16:11 +0100

    Added test for instantiating the lazy collection

--- tests/Unit/Core/Utilities/LazyCollectionTest.php
+++ tests/Unit/Core/Utilities/LazyCollectionTest.php
@@ -9,6 +9,16 @@ final class LazyCollectionTest extends \PHPUnit\Framework\TestCase
 {
     use MockeryPHPUnitIntegration;
 
+    public function testCanBeCreated()
+    {
+        $collection = new LazyCollection(function () {
+            for ($i = 0; $i < 5; $i++) {
+                yield $i;
+            }
+        });
+        $this->assertInstanceOf(LazyCollection::class, $collection);
+    }
+
     public function testCanBeCalledStatically()
     {
         $this->collection = LazyCollection::make(function () {
