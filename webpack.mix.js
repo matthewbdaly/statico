bba9ccee3dbd482ba75bb5f47215735990946967
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-11-20 08:49:43 +0000

    Upgraded Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,5 +18,5 @@ mix
   .sass('resources/sass/app.scss', 'public/css')
   .options({
     processCssUrls: false,
-    postCss: [ tailwindcss('./tailwind.js') ],
+    postCss: [ tailwindcss('./tailwind.config.js') ],
   }).version();
