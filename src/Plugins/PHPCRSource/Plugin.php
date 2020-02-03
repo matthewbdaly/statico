5b0499fa49a94fa93713517cb4cea17317afa07d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-02-03 21:45:45 +0000

    Set up console tasks

--- src/Plugins/PHPCRSource/Plugin.php
+++ src/Plugins/PHPCRSource/Plugin.php
@@ -7,6 +7,7 @@ namespace Statico\Plugins\PHPCRSource;
 use Statico\Core\Contracts\Plugin as PluginContract;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 final class Plugin implements PluginContract
 {
@@ -28,11 +29,39 @@ final class Plugin implements PluginContract
     public function register(): void
     {
         $this->registerServiceProvider();
+        $this->registerConsoleCommands();
     }
 
     private function registerServiceProvider(): void
     {
         $this->container->addServiceProvider('Statico\Plugins\PHPCRSource\Providers\DocumentManagerProvider');
         $this->container->addServiceProvider('Statico\Plugins\PHPCRSource\Providers\PHPCRRepositoryProvider');
+    }
+
+    private function registerConsoleCommands(): void
+    {
+        $this->console->addCommands([
+            new \PHPCR\Util\Console\Command\NodeDumpCommand(),
+            new \PHPCR\Util\Console\Command\NodeMoveCommand(),
+            new \PHPCR\Util\Console\Command\NodeRemoveCommand(),
+            new \PHPCR\Util\Console\Command\NodesUpdateCommand(),
+            new \PHPCR\Util\Console\Command\NodeTouchCommand(),
+            new \PHPCR\Util\Console\Command\NodeTypeListCommand(),
+            new \PHPCR\Util\Console\Command\NodeTypeRegisterCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceCreateCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceDeleteCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceExportCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceImportCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceListCommand(),
+            new \PHPCR\Util\Console\Command\WorkspacePurgeCommand(),
+            new \PHPCR\Util\Console\Command\WorkspaceQueryCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\DocumentMigrateClassCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\DocumentConvertTranslationCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\GenerateProxiesCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\DumpQueryBuilderReferenceCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\InfoDoctrineCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\VerifyUniqueNodeTypesMappingCommand(),
+            new \Doctrine\ODM\PHPCR\Tools\Console\Command\RegisterSystemNodeTypesCommand(),
+        ]);
     }
 }
